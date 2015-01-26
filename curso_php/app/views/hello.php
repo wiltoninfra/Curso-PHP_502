197960c67f995239b7c650a1ee258048999c0648
root <root@webservercloud.dexter.com.br>
2015-01-26 20:55:46 -0200

    Primeiro Commit

---
+++ curso_php/app/views/hello.php
@@ -0,0 +1,85 @@
+<?php
+$atual = new DateTime ();
+
+$especifica = new DateTime ( '1990-01-22' );
+
+$texto = new DateTime ( '+1 month' );
+
+print_r ( $atual );
+
+echo '<hr><br>';
+
+print_r ( $especifica );
+
+echo '<hr><br>';
+
+print_r ( $texto );
+
+echo '<hr><br>';
+
+echo '<h3>Formatando</h3>';
+
+echo '<p>';
+
+echo $atual->format( 'd-m-Y' );
+
+echo '<p>';
+
+$atual->setDate(1982, 12, 08);
+echo $atual->format('d-m-Y');
+
+echo '<p>';
+
+$atual->setTime(8, 1, 2);
+
+$atual->setDate(1982, 12, 08);
+
+echo $atual->format('d-m-Y h:i:s');
+
+
+echo '<p>';
+
+
+$fuso = new DateTimeZone('America/New_York');
+$atual->setTimezone($fuso);
+
+echo $atual->format('d-m-Y h:i:s');
+
+echo '<hr><br>';
+
+echo '<h3>Intervalo</h3>';
+
+echo '<p>';
+
+$intervalo = new DateInterval('P3YT8M');
+print_r($intervalo);
+
+echo '<p>';
+
+$intervalo1 = new DateInterval('P2YT8M');
+
+echo $intervalo1->format('%y anos e %d dias');
+
+
+echo '<p>';
+
+$data1 = new DateTime('2011-09-11'); 
+$data2 = new DateTime('2011-10-13');
+
+$intervalo = $data1->diff($data2);
+
+echo $intervalo->format('%R%a dias');
+
+echo '<p>';
+
+$data1 = new DateTime('2011-09-11');
+$data2 = new DateTime('2011-10-13');
+
+var_dump($data1 == $data2);
+var_dump($data1 > $data2);
+var_dump($data1 < $data2);
+
+
+
+
+
---
+++ exemplos_aula/Capitulo1/502_aula_1/date.php
@@ -0,0 +1,36 @@
+<?php
+
+// Exibe data e hora verifique as formatacoes
+
+echo date ('d/m/Y', strtotime("+1 month"));
+
+//Exibe Tempo pode se converter
+echo '<hr>';
+
+echo time();
+
+
+echo '<hr>';
+
+//Exibe em Microsegundos
+
+echo microtime();
+
+
+echo '<hr>';
+//Caculo em segundos desde a data e hora informada
+echo mktime(0, 0, 0, 01, 01, 02, 1982);
+
+
+
+echo '<hr>';
+//Converte uma string em na data por extenso
+
+echo strtotime("+1 month");
+		
+
+
+
+
+
+		
